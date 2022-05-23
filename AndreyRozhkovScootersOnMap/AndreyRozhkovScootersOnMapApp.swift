80988f2d06898e9c485355fa066219f0cbed6c44
Andrey Rozhkov <an.rozhkov@corp.mail.ru>
2022-05-23 20:28:22 +0200

    Initial Commit

---
+++ AndreyRozhkovScootersOnMap/AndreyRozhkovScootersOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndreyRozhkovScootersOnMapApp.swift
+//  AndreyRozhkovScootersOnMap
+//
+//  Created by an.rozhkov on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AndreyRozhkovScootersOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
